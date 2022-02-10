4e0ba28d84cbc45b3743a963531236f8dc0678da
SynFunjioka <alanff2800@gmail.com>
2022-02-10 12:43:38 -0600

    Fisrt push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import "./App.css";
+const PayPalButton = window.paypal.Buttons.driver("react", { React, ReactDOM });
 function App() {
+  function _createOrder(data, actions) {
+    return actions.order.create({
+      purchase_units: [
+        {
+          amount: {
+            value: "1",
+          },
+        },
+      ],
+    });
+  }
+  async function _onApprove(data, actions) {
+    let order = await actions.order.capture();
+    console.log(order);
+    window.ReactNativeWebView &&
+      window.ReactNativeWebView.postMessage(JSON.stringify(order));
+    return order;
+  }
+  function _onError(err) {
+    console.log(err);
+    let errObj = {
+      err: err,
+      status: "FAILED",
+    };
+    window.ReactNativeWebView &&
+      window.ReactNativeWebView.postMessage(JSON.stringify(errObj));
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PayPalButton
+        createOrder={(data, actions) => _createOrder(data, actions)}
+        onApprove={(data, actions) => _onApprove(data, actions)}
+        onCancel={() => _onError("CANCELED")}
+        onError={(err) => _onError("ERROE")}
+      />
     </div>
   );
 }
-
 export default App;
