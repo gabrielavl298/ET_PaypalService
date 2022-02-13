801863e02ffe3552d5842ddf1d31dacb48bc7e5b
SynFunjioka <alanff2800@gmail.com>
2022-02-12 19:05:34 -0600

    Paypal serve update

--- src/App.js
+++ src/App.js
@@ -1,28 +1,60 @@
-import React from "react";
-import ReactDOM from "react-dom";
 import "./App.css";
+import React, {useState} from "react";
+import ReactDOM from "react-dom";
 const PayPalButton = window.paypal.Buttons.driver("react", { React, ReactDOM });
+
 function App() {
+  const [price, setPrice] = useState("1")
+
   function _createOrder(data, actions) {
     return actions.order.create({
       purchase_units: [
         {
-          amount: {
-            value: "1",
+          "amount": {
+            "currency_code": "MXN",
+            "value": "250",
+            "breakdown": {
+              "item_total": {  /* Required when including the `items` array */
+                "currency_code": "MXN",
+                "value": "250"
+              }
+            }
           },
+          "items": [
+            {
+              "name": "Lentes", /* Shows within upper-right dropdown during payment approval */
+              "description": "Lentes de sol para los gamers", /* Item details will also be in the completed paypal.com transaction view */
+              "unit_amount": {
+                "currency_code": "MXN",
+                "value": "55"
+              },
+              "quantity": "2",
+              "id": "sdlkfjasdlfgk"
+            },
+            {
+              "name": "Condones", /* Shows within upper-right dropdown during payment approval */
+              "description": "Condones para los gamers", /* Item details will also be in the completed paypal.com transaction view */
+              "unit_amount": {
+                "currency_code": "MXN",
+                "value": "70"
+              },
+              "quantity": "2",
+              "id": "cvhjhdkjfgh"
+            },
+          ]
         },
       ],
     });
   }
   async function _onApprove(data, actions) {
     let order = await actions.order.capture();
-    console.log(order);
+    console.log("order", order);
     window.ReactNativeWebView &&
       window.ReactNativeWebView.postMessage(JSON.stringify(order));
     return order;
   }
   function _onError(err) {
-    console.log(err);
+    console.log("err", err);
     let errObj = {
       err: err,
       status: "FAILED",
@@ -32,11 +64,12 @@ function App() {
   }
   return (
     <div className="App">
+      <input type = "text" onChange={e => setPrice(e.target.value)} value = {price}/>
       <PayPalButton
         createOrder={(data, actions) => _createOrder(data, actions)}
         onApprove={(data, actions) => _onApprove(data, actions)}
         onCancel={() => _onError("CANCELED")}
-        onError={(err) => _onError("ERROE")}
+        onError={(err) => _onError("ERROR")}
       />
     </div>
   );
